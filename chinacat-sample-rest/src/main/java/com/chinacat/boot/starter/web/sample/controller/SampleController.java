d4522b2545edb007daa2ddb5d02630cf1e75d468
s.c.gao <s.c.gao@accenture.com>
2018-10-29 15:32:23 +0800

    Add RestResponse sample

--- chinacat-sample-rest/src/main/java/com/chinacat/boot/starter/web/sample/controller/SampleController.java
+++ chinacat-sample-rest/src/main/java/com/chinacat/boot/starter/web/sample/controller/SampleController.java
@@ -1,5 +1,6 @@
 package com.chinacat.boot.starter.web.sample.controller;
 
+import com.chinacat.boot.model.response.RestResponse;
 import com.chinacat.boot.starter.web.core.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,11 @@ public class SampleController {
     @RequestMapping(value="/hello", method= RequestMethod.GET)
     public String hello(@RequestParam("someone") String someone){
         return "Hello " + someone;
+    }
+
+    @RequestMapping(value="/hello1", method= RequestMethod.GET)
+    public RestResponse<String> hello1(@RequestParam("someone") String someone){
+        return RestResponse.<String>newInstance().success("Success", "Hello " + someone);
     }
 
 }
